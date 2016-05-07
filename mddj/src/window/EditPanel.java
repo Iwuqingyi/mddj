409afc088580e61d12b645d7d1649c11ce542f2b
Iwuqingyi <Iwuqingyi@gmail.com>
2016-05-07 18:19:26 +0800

    s1

--- mddj/src/window/EditPanel.java
+++ mddj/src/window/EditPanel.java
@@ -12,6 +12,7 @@ import java.awt.Rectangle;
 import javax.swing.JComboBox;
 import javax.swing.JCheckBox;
 import javax.swing.JTextArea;
+import java.awt.Dimension;
 
 public class EditPanel extends JPanel {
 	private JTextField textField;
@@ -32,25 +33,40 @@ public class EditPanel extends JPanel {
 	private JLabel lblNewLabel_9;
 	private JTextArea textArea;
 	private JTextArea textArea_1;
+	private JLabel lblNewLabel_10;
+	private JLabel label;
+	private JLabel label_1;
+	private JLabel label_2;
+	private JLabel lblNewLabel_11;
 
 	/**
 	 * Create the panel.
 	 */
 	public EditPanel() {
-		setSize(776,195);
+		setSize(782,196);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{60, 109, 48, 96, 65, 119, 0, 98, 62, 43, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 77, 58, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 39, 34, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
+		
+		lblNewLabel_11 = new JLabel("");
+		lblNewLabel_11.setMinimumSize(new Dimension(18, 20));
+		lblNewLabel_11.setMaximumSize(new Dimension(18, 20));
+		GridBagConstraints gbc_lblNewLabel_11 = new GridBagConstraints();
+		gbc_lblNewLabel_11.gridwidth = 10;
+		gbc_lblNewLabel_11.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_11.gridx = 0;
+		gbc_lblNewLabel_11.gridy = 0;
+		add(lblNewLabel_11, gbc_lblNewLabel_11);
 		
 		JLabel lblNewLabel = new JLabel("\u7F16        \u53F7");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 0;
+		gbc_lblNewLabel.gridy = 1;
 		add(lblNewLabel, gbc_lblNewLabel);
 		
 		textField = new JTextField();
@@ -58,16 +74,16 @@ public class EditPanel extends JPanel {
 		gbc_textField.insets = new Insets(0, 0, 5, 5);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 0;
+		gbc_textField.gridy = 1;
 		add(textField, gbc_textField);
 		textField.setColumns(10);
 		
-		lblNewLabel_3 = new JLabel("\u7B49\u7EA7");
+		lblNewLabel_3 = new JLabel("\u7B49     \u7EA7");
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
 		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_3.gridx = 2;
-		gbc_lblNewLabel_3.gridy = 0;
+		gbc_lblNewLabel_3.gridy = 1;
 		add(lblNewLabel_3, gbc_lblNewLabel_3);
 		
 		comboBox = new JComboBox();
@@ -75,7 +91,7 @@ public class EditPanel extends JPanel {
 		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
 		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox.gridx = 3;
-		gbc_comboBox.gridy = 0;
+		gbc_comboBox.gridy = 1;
 		add(comboBox, gbc_comboBox);
 		
 		JLabel lblNewLabel_1 = new JLabel("\u90E8 \u59D4 \u53F7");
@@ -83,7 +99,7 @@ public class EditPanel extends JPanel {
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_1.gridx = 4;
-		gbc_lblNewLabel_1.gridy = 0;
+		gbc_lblNewLabel_1.gridy = 1;
 		add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		textField_1 = new JTextField();
@@ -91,7 +107,7 @@ public class EditPanel extends JPanel {
 		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.gridx = 5;
-		gbc_textField_1.gridy = 0;
+		gbc_textField_1.gridy = 1;
 		add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
 		
@@ -100,7 +116,7 @@ public class EditPanel extends JPanel {
 		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_4.gridx = 6;
-		gbc_lblNewLabel_4.gridy = 0;
+		gbc_lblNewLabel_4.gridy = 1;
 		add(lblNewLabel_4, gbc_lblNewLabel_4);
 		
 		textField_3 = new JTextField();
@@ -108,7 +124,7 @@ public class EditPanel extends JPanel {
 		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_3.gridx = 7;
-		gbc_textField_3.gridy = 0;
+		gbc_textField_3.gridy = 1;
 		add(textField_3, gbc_textField_3);
 		textField_3.setColumns(10);
 		
@@ -117,7 +133,7 @@ public class EditPanel extends JPanel {
 		gbc_lblNewLabel_5.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_5.gridx = 8;
-		gbc_lblNewLabel_5.gridy = 0;
+		gbc_lblNewLabel_5.gridy = 1;
 		add(lblNewLabel_5, gbc_lblNewLabel_5);
 		
 		textField_4 = new JTextField();
@@ -125,7 +141,7 @@ public class EditPanel extends JPanel {
 		gbc_textField_4.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_4.gridx = 9;
-		gbc_textField_4.gridy = 0;
+		gbc_textField_4.gridy = 1;
 		add(textField_4, gbc_textField_4);
 		textField_4.setColumns(10);
 		
@@ -134,7 +150,7 @@ public class EditPanel extends JPanel {
 		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_2.gridx = 0;
-		gbc_lblNewLabel_2.gridy = 1;
+		gbc_lblNewLabel_2.gridy = 2;
 		add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
 		textField_2 = new JTextField();
@@ -143,7 +159,7 @@ public class EditPanel extends JPanel {
 		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_2.gridx = 1;
-		gbc_textField_2.gridy = 1;
+		gbc_textField_2.gridy = 2;
 		add(textField_2, gbc_textField_2);
 		textField_2.setColumns(10);
 		
@@ -152,7 +168,7 @@ public class EditPanel extends JPanel {
 		gbc_lblNewLabel_7.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_7.gridx = 4;
-		gbc_lblNewLabel_7.gridy = 1;
+		gbc_lblNewLabel_7.gridy = 2;
 		add(lblNewLabel_7, gbc_lblNewLabel_7);
 		
 		textField_5 = new JTextField();
@@ -161,7 +177,7 @@ public class EditPanel extends JPanel {
 		gbc_textField_5.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_5.gridx = 5;
-		gbc_textField_5.gridy = 1;
+		gbc_textField_5.gridy = 2;
 		add(textField_5, gbc_textField_5);
 		textField_5.setColumns(10);
 		
@@ -170,7 +186,7 @@ public class EditPanel extends JPanel {
 		gbc_chckbxNewCheckBox.anchor = GridBagConstraints.EAST;
 		gbc_chckbxNewCheckBox.insets = new Insets(0, 0, 5, 5);
 		gbc_chckbxNewCheckBox.gridx = 7;
-		gbc_chckbxNewCheckBox.gridy = 1;
+		gbc_chckbxNewCheckBox.gridy = 2;
 		add(chckbxNewCheckBox, gbc_chckbxNewCheckBox);
 		
 		lblNewLabel_6 = new JLabel("\u5355\u4F4D\u6570");
@@ -178,7 +194,7 @@ public class EditPanel extends JPanel {
 		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_6.gridx = 8;
-		gbc_lblNewLabel_6.gridy = 1;
+		gbc_lblNewLabel_6.gridy = 2;
 		add(lblNewLabel_6, gbc_lblNewLabel_6);
 		
 		textField_6 = new JTextField();
@@ -186,48 +202,78 @@ public class EditPanel extends JPanel {
 		gbc_textField_6.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_6.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_6.gridx = 9;
-		gbc_textField_6.gridy = 1;
+		gbc_textField_6.gridy = 2;
 		add(textField_6, gbc_textField_6);
 		textField_6.setColumns(10);
 		
-		lblNewLabel_8 = new JLabel("\u6807        \u9898");
-		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
-		gbc_lblNewLabel_8.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_8.gridheight = 2;
-		gbc_lblNewLabel_8.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_8.insets = new Insets(0, 0, 0, 5);
-		gbc_lblNewLabel_8.gridx = 0;
-		gbc_lblNewLabel_8.gridy = 2;
-		add(lblNewLabel_8, gbc_lblNewLabel_8);
+		lblNewLabel_10 = new JLabel("\u6807");
+		GridBagConstraints gbc_lblNewLabel_10 = new GridBagConstraints();
+		gbc_lblNewLabel_10.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_10.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_10.gridx = 0;
+		gbc_lblNewLabel_10.gridy = 3;
+		add(lblNewLabel_10, gbc_lblNewLabel_10);
 		
 		textArea = new JTextArea();
 		GridBagConstraints gbc_textArea = new GridBagConstraints();
-		gbc_textArea.gridheight = 2;
+		gbc_textArea.gridheight = 4;
 		gbc_textArea.gridwidth = 4;
 		gbc_textArea.insets = new Insets(0, 0, 0, 5);
 		gbc_textArea.fill = GridBagConstraints.BOTH;
 		gbc_textArea.gridx = 1;
-		gbc_textArea.gridy = 2;
+		gbc_textArea.gridy = 3;
 		add(textArea, gbc_textArea);
 		
-		lblNewLabel_9 = new JLabel("\u529E\u7406\u60C5\u51B5");
+		label = new JLabel("\u529E");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.anchor = GridBagConstraints.EAST;
+		gbc_label.insets = new Insets(0, 0, 5, 5);
+		gbc_label.gridx = 5;
+		gbc_label.gridy = 3;
+		add(label, gbc_label);
+		
+		label_1 = new JLabel("\u7406");
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.anchor = GridBagConstraints.EAST;
+		gbc_label_1.insets = new Insets(0, 0, 5, 5);
+		gbc_label_1.gridx = 5;
+		gbc_label_1.gridy = 4;
+		add(label_1, gbc_label_1);
+		
+		lblNewLabel_9 = new JLabel("\u60C5");
 		GridBagConstraints gbc_lblNewLabel_9 = new GridBagConstraints();
 		gbc_lblNewLabel_9.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_9.gridheight = 2;
 		gbc_lblNewLabel_9.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_9.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_9.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_9.gridx = 5;
-		gbc_lblNewLabel_9.gridy = 2;
+		gbc_lblNewLabel_9.gridy = 5;
 		add(lblNewLabel_9, gbc_lblNewLabel_9);
 		
 		textArea_1 = new JTextArea();
 		GridBagConstraints gbc_textArea_1 = new GridBagConstraints();
-		gbc_textArea_1.gridheight = 2;
+		gbc_textArea_1.gridheight = 4;
 		gbc_textArea_1.gridwidth = 4;
 		gbc_textArea_1.fill = GridBagConstraints.BOTH;
 		gbc_textArea_1.gridx = 6;
-		gbc_textArea_1.gridy = 2;
+		gbc_textArea_1.gridy = 3;
 		add(textArea_1, gbc_textArea_1);
+		
+		lblNewLabel_8 = new JLabel("\u9898");
+		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+		gbc_lblNewLabel_8.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_8.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_8.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_8.gridx = 0;
+		gbc_lblNewLabel_8.gridy = 6;
+		add(lblNewLabel_8, gbc_lblNewLabel_8);
+		
+		label_2 = new JLabel("\u51B5");
+		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+		gbc_label_2.anchor = GridBagConstraints.EAST;
+		gbc_label_2.insets = new Insets(0, 0, 0, 5);
+		gbc_label_2.gridx = 5;
+		gbc_label_2.gridy = 6;
+		add(label_2, gbc_label_2);
 
 	}
 
